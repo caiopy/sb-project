54d3af50bcf65ead06db82ca84c51d9e2230672c
caiopy <caiop.sci@hotmail.com>
2023-01-05 21:01:24 -0300

    initial commit

---
+++ src/test/java/com/caio/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
