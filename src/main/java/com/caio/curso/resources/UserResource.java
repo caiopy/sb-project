dd98bd0cfd5556a050561783105e8a9cad379043
caiopy <caiop.sci@hotmail.com>
2023-01-09 09:36:08 -0300

    Service layer, Component registration

--- src/main/java/com/caio/curso/resources/UserResource.java
+++ src/main/java/com/caio/curso/resources/UserResource.java
@@ -1,22 +1,35 @@
 package com.caio.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caio.curso.entities.User;
+import com.caio.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
 		
-		
-		User u = new User(1L,"teste","teste@gmail.com","99999999","password");
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
