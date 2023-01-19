3920856501cfc0bbba6c6e4179c10401c74517ca
caiopy <caiop.sci@hotmail.com>
2023-01-19 08:30:07 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/caio/curso/config/TestConfig.java
+++ src/main/java/com/caio/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.caio.curso.entities.Category;
 import com.caio.curso.entities.Order;
+import com.caio.curso.entities.OrderItem;
 import com.caio.curso.entities.Product;
 import com.caio.curso.entities.User;
 import com.caio.curso.entities.enums.OrderStatus;
 import com.caio.curso.repositories.CategoryRepository;
+import com.caio.curso.repositories.OrderItemRepository;
 import com.caio.curso.repositories.OrderRepository;
 import com.caio.curso.repositories.ProductRepository;
 import com.caio.curso.repositories.UserRepository;
@@ -33,7 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
-
+	
+	@Autowired 
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,6 +76,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
